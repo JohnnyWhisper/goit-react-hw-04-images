1f8a535267e434b2cbb7d2b7317b0dcb04b15f2f
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-07-28 00:19:24 +0300

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -13,7 +13,7 @@ const Button = ({ onClickLoadMore }) => {
   );
 };
 Button.propTypes = {
-  onLoadMore: PropTypes.func.isRequired,
+  onClickLoadMore: PropTypes.func.isRequired,
 };
 
 export default Button;
